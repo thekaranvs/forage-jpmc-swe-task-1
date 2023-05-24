52319db4b65dd25993c1abe19af7f2af0697aba6
Karanveer Singh <karanvs@Karanveers-MacBook-Air.local>
2023-05-24 08:52:29 +0800

    fix client3.py and update tests

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +8,8 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price'])/2))
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,9 +17,24 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote), (quote['stock'], quote['top_bid']['price'], quote['top_ask']['price'], (quote['top_bid']['price'] + quote['top_ask']['price'])/2))
 
   """ ------------ Add more unit tests ------------ """
+  def test_getRatio_calculateRatio(self):
+    prices_a = [121.2, 121.68, 117.87, 119.8]
+    prices_b = [98.8, 101.97, 95.4, 103.5]
+    '''Assertions Below'''
+    for index in range(len(prices_a)):
+      self.assertEqual(getRatio(prices_a[index], prices_b[index]), (prices_a[index] / prices_b[index]))
+    
+  def test_getRatio_priceBZero(self):
+    price_a, price_b = 104.7, 0
+    self.assertIsNone(getRatio(price_a, price_b))
+  
+  def test_getRatio_priceAZero(self):
+    price_a, price_b = 0, 105.8
+    self.assertEqual(0, getRatio(price_a, price_b))
 
 
 
